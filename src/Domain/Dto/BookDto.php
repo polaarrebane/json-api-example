52b7610c7f01bcae78d231ecfb98fc5adf565d16
Roni Rebane <roni.rebane@gmail.com>
2024-02-25 11:57:40 +0400

    feat: add DtoInterface::class

--- src/Domain/Dto/BookDto.php
+++ src/Domain/Dto/BookDto.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Domain\Dto;
 
-readonly class BookDto
+readonly class BookDto implements DtoInterface
 {
     public function __construct(
         public string $id,
